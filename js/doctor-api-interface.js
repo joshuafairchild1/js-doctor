a5c3296cf6ca2b56b4378580f8cc4ff272e4e4bf
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-06-30 12:51:34 -0700

    add more the the ailmentSearch method so that it now gets specific pieces of information about the found doctor, and passes them into a front end function that will render them on the page

--- js/doctor-api-interface.js
+++ js/doctor-api-interface.js
@@ -1,11 +1,14 @@
 const DoctorFinder = require('./../js/doctor-api.js').doctorFinderModule;
 
+const displayDoctor = (doctor, address) => $("#doctors").append(`<li>${doctor} at ${address}</li>`);
+
 $(() => {
   const doctorFinder = new DoctorFinder();
 
   $("#ailment-form").submit(ev => {
     ev.preventDefault();
+    $("#doctors").empty();
     const ailment = $("#ailment-input").val();
-    doctorFinder.ailmentSearch(ailment);
+    doctorFinder.ailmentSearch(ailment, displayDoctor);
   });
 });
--- js/doctor-api.js
+++ js/doctor-api.js
@@ -1,9 +1,19 @@
 const apiKey = require('./../.env').apiKey;
 
 class DoctorFinder {
-  ailmentSearch(ailment) {
+  ailmentSearch(ailment, displayFn) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query= ${ailment}+&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=${apiKey}`)
-      .then(response => console.log(response))
+      .then(response => {
+        console.log(response);
+        const doctors = response.data;
+        doctors.forEach(doctor => {
+          const profile = doctor.profile;
+          const location = doctor.practices[0].visit_address;
+          const doctorName = `${profile.first_name} ${profile.last_name}, ${profile.title}`;
+          const address = `${location.street}, ${location.city} ${location.state} ${location.zip}`;
+          displayFn(doctorName, address);
+        });
+      })
       .catch(error => console.log(error));
   }
 }
